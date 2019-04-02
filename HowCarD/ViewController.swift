68c342fec5ade6e1132d7602831632dad57fab0d
lohsts <lohsts@gmail.com>
2019-04-02 14:03:29 +0800

    Add Project.

---
+++ HowCarD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowCarD
+//
+//  Created by lohsts on 2019/4/2.
+//  Copyright © 2019 lohsts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
