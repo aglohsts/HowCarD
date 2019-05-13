a24794b398ff6420dc3b5273c67a596d9077d09e
lohsts <lohsts@gmail.com>
2019-05-13 18:34:22 +0800

    Modify BillDueDateTableViewCell and BillRemindTableViewCell.

--- HowCarD+/View/Wallet/BillDueDateTableViewCell.swift
+++ HowCarD+/View/Wallet/BillDueDateTableViewCell.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class BillDueDateTableViewCell: UITableViewCell {
+    
+    let dates: [ Int ] =
+        [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+          11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    ]
 
+    @IBOutlet weak var billDueDateTextField: UITextField!
+    
+    var needBillRemind: Bool = true {
+        
+        didSet {
+            
+            if needBillRemind {
+                
+                billDueDateTextField.isUserInteractionEnabled = true
+                
+            } else {
+                
+                billDueDateTextField.isUserInteractionEnabled = false
+                
+                billDueDateTextField.text = nil
+            }
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +46,55 @@ class BillDueDateTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+extension BillDueDateTableViewCell {
+    
+    func layoutCell(dueDate: Int?, needBillRemind: Bool) {
+        
+        if dueDate == nil {
+            
+            billDueDateTextField.text = nil
+        } else {
+            
+            billDueDateTextField.text = String(dueDate!)
+        }
+        
+        self.needBillRemind = needBillRemind
+    }
+    
+    func createPickerView() {
+        
+        let pickerView = UIPickerView()
+        
+        pickerView.delegate = self
+        
+        billDueDateTextField.inputView = pickerView
+    }
+}
+
+extension BillDueDateTableViewCell: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return String(dates[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        billDueDateTextField.text = String(dates[row])
+    }
+}
+
+extension BillDueDateTableViewCell: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return dates.count
+    }
 }
--- HowCarD+/View/Wallet/BillRemindTableViewCell.swift
+++ HowCarD+/View/Wallet/BillRemindTableViewCell.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class BillRemindTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var billRemindSwitch: UISwitch!
+    
+    var needBillRemind: Bool = true {
+        
+        didSet {
+            
+            if needBillRemind {
+                
+                selectedDate = 1
+            } else {
+                
+                selectedDate = nil
+            }
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,5 +36,46 @@ class BillRemindTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    @IBAction func onBillRemindSwitch(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            
+//            billDueDateTextField.isUserInteractionEnabled = true
+//
+//            billDueDateView.backgroundColor = UIColor.tint?.withAlphaComponent(1.0)
+            
+            needBillRemind = true
+            
+//            selectedDate = 1
+//
+//            guard let dueDate = selectedDate else { return }
+//
+//            billDueDateTextField.text = String(dueDate)
+            
+            //Notification Center
+        } else {
+            
+//            billDueDateTextField.isUserInteractionEnabled = false
+//
+//            billDueDateView.backgroundColor = UIColor.tint?.withAlphaComponent(0.5)
+            
+            needBillRemind = false
+            
+//            selectedDate = nil
+//
+//            billDueDateTextField.text = nil
+            
+            //Notification Center
+        }
+    }
+    
+    func layoutCell(needBillRemind: Bool) {
+        
+        self.needBillRemind = needBillRemind
+    }
 }
+
+
+
+
